20e4cf8ea60d29dddd8b6801e1c5ffb7ab32e86d
bitsybaron <59240206+bitsybaron@users.noreply.github.com>
2020-07-22 14:58:54 -0600

    finished app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Components/Image'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Image url={'https://www.caa.com/sites/default/files/styles/headshot_500x500/public/2020-02/Mindy-Kaling_0.jpg?itok=iEc5gokl'}/>
+      </div>
+    )
+  }
 }
 
 export default App;
