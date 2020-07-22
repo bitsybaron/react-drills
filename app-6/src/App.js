37450df5c87552a760b7bde568e940701a3c557a
bitsybaron <59240206+bitsybaron@users.noreply.github.com>
2020-07-22 15:42:51 -0600

    hack version of app-6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Components/Todo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        userInput: '',
+        list: []
+    }
+  }
+  handleChange(val) {
+      this.setState({
+        userInput: val
+      })
+  }
+  addTask(val) {
+    let newList = this.state.list;
+    newList.push(val)
+    this.setState({
+      list: newList
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Tasks</h1>
+        <input onChange={e => this.handleChange(e.target.value)}/>
+        <button onClick={e => this.addTask(this.state.userInput)}>Add Task</button>
+        <Todo listy={this.state.list}/>
+      </div>
+    )
+  }
 }
 
 export default App;
